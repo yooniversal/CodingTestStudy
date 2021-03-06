ac46accd1195c35c474482fa61858fe5b41c848b
cd06114 <74724945+cd06114@users.noreply.github.com>
2021-03-06 09:04:27 +0900

    Djistra

--- BOJ/hooney/1753.cpp
+++ BOJ/hooney/1753.cpp
@@ -1,77 +1,72 @@
+#include <stdio.h>
 #include<iostream>
-#include<string.h>
 #include<vector>
-
+#include<queue>
+#define MAX 10000000
 using namespace std;
-int arr[20002][20002];
+vector<pair<int, int>> arr[20002];
 int cost[20002];
-#define MAX 11
+int check[20002];
 int V, E;
-void minimum_cost(int curr,int curr_cost)
-{
-	//cout << curr;
-
-	for (int i = 1; i <= V; i++)
+struct compare {
+	bool operator()(pair<int, int> a, pair <int, int> b)
 	{
-		//엣지가 있는 것 중에
-		if (arr[curr][i] != MAX)
+		return a.second > b.second;
+	}
+};
+void fucking_dijsktra(int s)
+{
+	/*for (int i = 0; i < arr[s].size(); i++)
+	{
+		cost[arr[s][i].first] = arr[s][i].second;
+	}*/
+
+	priority_queue<pair<int, int>, vector<pair<int, int>>, compare>pq;
+	cost[s] = 0;
+	pq.push({ s,0 });
+
+	while (!pq.empty())
+	{
+		int current_position = pq.top().first;
+		int distance = pq.top().second;
+		pq.pop();
+		//cout << current_position << " " << distance << endl;
+		if (distance > cost[current_position])continue;
+
+		for (int i = 0; i < arr[current_position].size(); i++)
 		{
-			//현재 가지고 있는 비용보다 지금 이동하는 것이 더 이덕젹일 경우
-			if (cost[i] > curr_cost + arr[curr][i])
+			int next = arr[current_position][i].first;
+			int nextDistance = arr[current_position][i].second + distance;
+			if (nextDistance < cost[next])
 			{
-				cost[i] = curr_cost + arr[curr][i];
+				cost[next] = nextDistance;
+				pq.push({ next,nextDistance });
 			}
 		}
 	}
-	int min=11;
-	for (int i = 1; i <= V; i++)
-	{
-		if (arr[curr][i] == 0)continue;
-		min = min > arr[curr][i] ? arr[curr][i] : min;
-	}
-	
-	if (min == 11)return;
-	//cout << min << endl;
-	minimum_cost(min,cost[min]);
+
+
 }
-int main()
+int main(void)
 {
-
-	cin >> V >> E;
 	int start;
-	cin >> start;
-	//최단 비용을 MAX로 초기화
-	for (int i = 1; i <= V; i++)
-	{
-		cost[i] = MAX;
-	}
+	scanf("%d %d", &V, &E);
+	scanf("%d", &start);
 
-	// j->i로 가는 비용 초기화
-	for (int j = 1; j <= V; j++)
-	{
-		for (int i = 1; i <= V; i++)
-		{
-			arr[j][i] = MAX;
-		}
-		arr[j][j] = 0;
-	}
-
-	//각 노드 입력받기
 	for (int i = 0; i < E; i++)
 	{
 		int a, b, c;
-		cin >> a >> b >> c;
-		arr[a][b] = c;
+		scanf("%d %d %d", &a, &b, &c);
+		arr[a].push_back({ b,c });
 	}
-
-
-	cost[start] = 0;
-	minimum_cost(start,0);
-	
+	for (int i = 0; i <= V; i++)
+	{
+		cost[i] = MAX;
+	}
+	fucking_dijsktra(start);
 	for (int i = 1; i <= V; i++)
 	{
-		if (cost[i] == 11) { cout << "INF" << endl; continue; }
-		cout << cost[i] << endl;
+		if (cost[i] == MAX) { printf("INF\n"); continue; }
+		printf("%d\n", cost[i]);
 	}
-
 }
