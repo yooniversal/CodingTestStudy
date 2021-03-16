559af7e46e7afa3c330b1f381cf3c7f379130d88
cd06114 <74724945+cd06114@users.noreply.github.com>
2021-03-16 16:33:41 +0900

    assn

--- BOJ/hooney/1753.cpp
+++ BOJ/hooney/1753.cpp
@@ -3,70 +3,30 @@
 #include<vector>
 #include<queue>
 #define MAX 10000000
+int arr[1000000];
 using namespace std;
-vector<pair<int, int>> arr[20002];
-int cost[20002];
-int check[20002];
-int V, E;
-struct compare {
-	bool operator()(pair<int, int> a, pair <int, int> b)
-	{
-		return a.second > b.second;
-	}
-};
-void fucking_dijsktra(int s)
+int cnt;
+int ret=1000000;
+int mini(int n)
 {
-	/*for (int i = 0; i < arr[s].size(); i++)
+	for (int i = 2; i <= n; i++)
 	{
-		cost[arr[s][i].first] = arr[s][i].second;
-	}*/
+		arr[i] = arr[i - 1] + 1;
+		if (arr[i] % 3 == 0)arr[i] = min(arr[i], arr[i / 3] + 1);
+		if (arr[i] % 2 == 0)arr[i] = min(arr[i], arr[i / 2] + 1);
 
-	priority_queue<pair<int, int>, vector<pair<int, int>>, compare>pq;
-	cost[s] = 0;
-	pq.push({ s,0 });
-
-	while (!pq.empty())
-	{
-		int current_position = pq.top().first;
-		int distance = pq.top().second;
-		pq.pop();
-		//cout << current_position << " " << distance << endl;
-		if (distance > cost[current_position])continue;
-
-		for (int i = 0; i < arr[current_position].size(); i++)
-		{
-			int next = arr[current_position][i].first;
-			int nextDistance = arr[current_position][i].second + distance;
-			if (nextDistance < cost[next])
-			{
-				cost[next] = nextDistance;
-				pq.push({ next,nextDistance });
-			}
-		}
+		
 	}
-
-
+	return arr[n];
 }
 int main(void)
 {
-	int start;
-	scanf("%d %d", &V, &E);
-	scanf("%d", &start);
-
-	for (int i = 0; i < E; i++)
+	int x;
+	cin >> x;
+	for (int i = 1; i <= x; i++)
 	{
-		int a, b, c;
-		scanf("%d %d %d", &a, &b, &c);
-		arr[a].push_back({ b,c });
+		arr[i] = -1;
 	}
-	for (int i = 0; i <= V; i++)
-	{
-		cost[i] = MAX;
-	}
-	fucking_dijsktra(start);
-	for (int i = 1; i <= V; i++)
-	{
-		if (cost[i] == MAX) { printf("INF\n"); continue; }
-		printf("%d\n", cost[i]);
-	}
+	arr[1] = 0;
+	cout << mini(x);
 }
